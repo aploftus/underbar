251f7b7a40734289ccf634d304137d9dd629eddd
aploftus <aploftus@users.noreply.github.com>
2017-09-08 15:41:13 -0700

    Update _.reject to pass tests

--- src/underbar.js
+++ src/underbar.js
@@ -97,6 +97,14 @@
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    var result = [];
+
+    _.each(collection, function(item) {
+      if (!test(item)) {
+        result.push(item);
+      }
+    });
+    return result;
   };
 
   // Produce a duplicate-free version of the array.
