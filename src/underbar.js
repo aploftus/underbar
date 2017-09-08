3d9d6727f3e38a40a3702c422381b0c803a9813b
aploftus <aploftus@users.noreply.github.com>
2017-09-08 16:10:14 -0700

    Update reduce to pass tests

--- src/underbar.js
+++ src/underbar.js
@@ -116,6 +116,12 @@
 
   // Return the results of applying an iterator to each element.
   _.map = function(collection, iterator) {
+    var result = [];
+
+    _.each(collection, function(item) {
+      result.push(iterator(item));
+    });
+    return result;
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
@@ -160,6 +166,18 @@
   //   }); // should be 5, regardless of the iterator function passed in
   //          No accumulator is given so the first element is used.
   _.reduce = function(collection, iterator, accumulator) {
+    var acc = accumulator;
+    var coll = collection;
+
+    if (acc === undefined) {
+      acc = coll[0];
+      coll = coll.slice(1);
+    }
+
+    _.each(coll, function(item) {
+      acc = iterator(acc, item);
+    });
+    return acc;
   };
 
   // Determine if the array or object contains a given value (using `===`).
